784d882d49bc2aa8508c5cbe04e0e25ffef3270b
Michael <michaelbahng999@gmail.com>
2017-03-25 17:19:25 -0400

    fix up tablegenerator

--- src/TableGenerator.js
+++ src/TableGenerator.js
@@ -12,25 +12,26 @@ class TableGenerator {
   }
 
   columnTags() {
-    return Array.from(this.schema.columns).map(column => `<th>${column}</th>`);
+    return Array.from(this.schema.columns).map(column => `<th>${column}</th>`).join('\n');
   }
 
   tableHeaderTag() {
-    return `<tr>${this.columnTags.join('')}</tr>`
+    return `<tr>${this.columnTags()}</tr>`
   }
 
   tableId() {
     return `#${this.schema.tableName}`;
   }
 
-  tableTag() {
-    return pretty(`<table id="${this.tableId}">${this.tableHeaderTag}<tbody></tbody></table>`);
+  tableTag(isPretty = false) {
+    const table = `<table id="${this.tableId()}">${this.tableHeaderTag()}<tbody></tbody></table>`
+    return isPretty ? pretty(table) : table;
   }
 
   tableRow(row) {
     return Array.from(this.schema.columns).map(column => {
       return `<td data-column"${column}">${row[column] || ''}</td>`
-    });
+    }).join('\n');
   }
   
 }
