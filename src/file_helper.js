3cd81c20c411805656bffc69570e28ce3acae759
Michael <michaelbahng999@gmail.com>
2017-03-25 17:52:31 -0400

    style

--- src/file_helper.js
+++ src/file_helper.js
@@ -1,13 +1,23 @@
 const cheerio = require('cheerio');
 const fs = require('fs');
+const pretty = require('pretty');
 
 const saveHtmlContent = (fileName, contents) => {
     fs.writeFileSync(fileName, contents);
 }
 
+const style = `<style>
+    html, body {
+        font-family: Comic Sans MS;
+    }
+    th, td {
+        border: 0.1px solid black;
+    }
+    </style>`
+
 const getHtmlContent = (fileName) => {
     if (!fs.existsSync(fileName)) {
-        saveHtmlContent(fileName, "<!DOCTYPE html><html><body></body></html>");
+        saveHtmlContent(fileName, pretty(`<!DOCTYPE html><html><head>${style}</head><body></body></html>`));
     }
     const contents = fs.readFileSync('./test.html', 'utf8').toString();
     return cheerio.load(contents);
